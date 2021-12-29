593de7b442331132e45018005bf09cfdc11330eb
Enderrrr-TW <77744486+Enderrrr-TW@users.noreply.github.com>
2021-12-28 22:36:34 -0500

    Delete check_stupid_space.py

--- check_stupid_space.py
+++
@@ -1,8 +0,0 @@
-import os
-flist=os.listdir('H:/Ender/boresight_calibration/20210513_india/vnir/processed/img_obs')
-for i in flist:
-    with open(i,'r') as f:
-        text=f.read()
-        if ' ' in text:
-            print(i)
-    
