aeffc4265ec28926b2f99b48019e3374719f5bb4
Enderrrr-TW <77744486+Enderrrr-TW@users.noreply.github.com>
2022-01-01 15:17:42 -0500

    Create test_regression.py
    This version doesn't work.

---
+++ LAI/PointNet_regression/test_regression.py
@@ -0,0 +1,103 @@
+"""
+Author: Benny
+Date: Nov 2019
+---
+Revised by An-Te Huang
+Date: Dec 31 2021
+Purpose: Create a script for regression
+"""
+from data_utils.xyzloader import LASDataLoader
+import argparse
+import numpy as np
+import os
+import torch
+import logging
+from tqdm import tqdm
+import sys
+import importlib
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = BASE_DIR
+sys.path.append(os.path.join(ROOT_DIR, 'models'))
+
+
+def parse_args():
+    '''PARAMETERS'''
+    parser = argparse.ArgumentParser('Testing')
+    parser.add_argument('--use_cpu', action='store_true', default=False, help='use cpu mode')
+    parser.add_argument('--gpu', type=str, default='0', help='specify gpu device')
+    parser.add_argument('--batch_size', type=int, default=24, help='batch size in training')
+    parser.add_argument('--num_category', default=40, type=int, choices=[10, 40],  help='training on ModelNet10/40')
+    parser.add_argument('--num_point', type=int, default=1024, help='Point Number')
+    parser.add_argument('--log_dir', type=str, required=True, help='Experiment root')
+    parser.add_argument('--use_normals', action='store_true', default=False, help='use normals')
+    parser.add_argument('--use_uniform_sample', action='store_true', default=False, help='use uniform sampiling')
+    parser.add_argument('--num_votes', type=int, default=3, help='Aggregate classification scores with voting')
+    return parser.parse_args()
+
+
+def test(model,loader, num_class=40, vote_num=1):
+    classifier = model.eval()
+
+    for j, (points, target) in tqdm(enumerate(loader), total=len(loader)):
+        if not args.use_cpu:
+            points, target = points.cuda(), target.cuda()
+        print(points.shape)
+        pred, _ = classifier(points)
+        print(pred.shape)
+        print(target.shape)
+        quit()
+        loss=torch.nn.MSELoss(pred,target)
+        
+    return loss
+
+
+def main(args):
+    def log_string(str):
+        logger.info(str)
+        print(str)
+
+    '''HYPER PARAMETER'''
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+
+    '''CREATE DIR'''
+    experiment_dir = 'log/classification/' + args.log_dir
+
+    '''LOG'''
+    args = parse_args()
+    logger = logging.getLogger("Model")
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler = logging.FileHandler('%s/eval.txt' % experiment_dir)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    log_string('PARAMETER ...')
+    log_string(args)
+
+    '''DATA LOADING'''
+    log_string('Load dataset ...')
+    data_path = 'remember to change this'
+
+    test_dataset = LASDataLoader(root=data_path, split='test', preprocessed=True)
+    testDataLoader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=10)
+
+    '''MODEL LOADING'''
+    num_class = 1
+    model_name = os.listdir(experiment_dir + '/logs')[0].split('.')[0]
+    model = importlib.import_module(model_name)
+
+    classifier = model.get_model(num_class, normal_channel=args.use_normals)
+    if not args.use_cpu:
+        classifier = classifier.cuda()
+    checkpoint = torch.load(str(experiment_dir) + '/checkpoints/best_model.pth')
+    classifier.load_state_dict(checkpoint['model_state_dict'])
+
+    with torch.no_grad():
+        test_loss = test(classifier.eval(), testDataLoader, vote_num=args.num_votes, num_class=num_class)
+        log_string('Test mean squared error: %f' % (test_loss))
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
